0a456c1e09c67fb6c97876c617b97c72974b0c60
elehmzzz <elehmzzz@gmail.com>
2026-06-26 09:09:41 -0600

    Añadir clase abstracta clsVehiculo al proyecto
    Se ha añadido la clase abstracta `clsVehiculo` en la carpeta
    `Clases`, la cual incluye un campo protegido `horasEstancia`,
    un constructor y un método abstracto `CalcularTarifa` para
    implementar en clases derivadas.
    
    Se actualizó el archivo del proyecto `GestionCobroEstacionamiento.csproj`
    para incluir la referencia al nuevo archivo `Clases\clsVehiculo.cs`.
    Además, se eliminó un grupo `<ItemGroup>` que contenía una carpeta
    vacía `Clases\` y se reemplazó por un grupo vacío `<ItemGroup />`.
    
    Se añadieron los espacios de nombres necesarios en `clsVehiculo.cs`
    para soportar las funcionalidades de la nueva clase.

---
+++ Clases/clsVehiculo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestionCobroEstacionamiento.Clases
+{
+    internal abstract class clsVehiculo
+    {
+        protected int horasEstancia;
+
+        //constructor
+        clsVehiculo(int horasEstancia) {
+            this.horasEstancia = horasEstancia;
+        }
+        public abstract float CalcularTarifa();
+    }
+}
